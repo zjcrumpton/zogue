dc429ee68dbfdb7fa81b84f92553c9e28a58ce86
zjcrumpton <Zaccary.Crumpton@charter.com>
2022-06-19 09:30:39 -0600

    update window size

--- include/Platform.hpp
+++ include/Platform.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
+// 40 x 22.5 tile screens = 800x640
 namespace Platform {
-    const int SCREEN_WIDTH = 640;
-    const int SCREEN_HEIGHT = 480;
+    const int SCREEN_WIDTH = 1280;
+    const int SCREEN_HEIGHT = 720;
 }
